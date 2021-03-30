4239842ca94a9c774fe9f48ac07d7cee117c2500
Racz Kerry <raczkerry@gmail.com>
2021-03-30 12:13:36 +0200

    Check if the first argument is an integer
    Exit with status 1 if not, otherwise continue

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write you code here
+const { positiveNumberRegex } = require('./utils/regexes')
+
+const filmNumber = process.argv[2]
+
+if (!positiveNumberRegex.test(filmNumber)) {
+  console.log('The first argument must be a whole number greater than 0')
+
+  process.exit(1)
+}
+
+console.log('OK')
