d353f467d9ed9543756ddb220a0e9336e1aa3f99
Racz Kerry <raczkerry@gmail.com>
2021-03-30 13:28:38 +0200

    Colorize the error output
    Add chalk dependency

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { positiveNumberRegex } = require('./utils/regexes')
 const { hasMountains, hasWaterSurface } = require('./utils/swapi')
 const API = require('./api')
+const chalk = require('chalk')
 
 const filmId = process.argv[2]
 
 if (!positiveNumberRegex.test(filmId)) {
-  console.log('The first argument must be a whole number greater than 0')
+  console.log(chalk.red('The first argument must be a whole number greater than 0'))
 
   process.exit(1)
 }
@@ -13,16 +14,16 @@ if (!positiveNumberRegex.test(filmId)) {
 const sumOfDiameter = async filmId => {
   const film = await API.Swapi.getFilm(filmId)
   if (!film) {
-    console.log('An error occured while retreiving the film informations')
-    console.log('Please ensure that the film id is correct')
+    console.log(chalk.red('An error occured while retreiving the film information'))
+    console.log(chalk.red('Please ensure that the film id is correct'))
 
     process.exit(1)
   }
 
   const planets = await Promise.all(film.planets.map(planetUrl => API.Swapi.getPlanet(planetUrl)))
-  if (!planets.some(planet => !planet)) {
-    console.log('An error occured while retreiving the planets informations')
-    console.log('This could be a problem with the swapi API, try later')
+  if (planets.some(planet => !planet)) {
+    console.log(chalk.red('An error occured while retreiving the planets information'))
+    console.log(chalk.red('This could be a problem with the swapi API, try later'))
 
     process.exit(1)
   }
