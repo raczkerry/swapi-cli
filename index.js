4b46d1947a7f190e8bcd1c3e1049d0cc9d39778b
Racz Kerry <raczkerry@gmail.com>
2021-03-31 13:27:40 +0200

    Refactor sumOfDiameter and use only the selected filters
    Call sumOfDiameter instead of loging the selected filters

--- index.js
+++ index.js
@@ -30,7 +30,36 @@ const cli = async filmId => {
     process.exit(1)
   }
 
-  console.log(answers)
+  sumOfDiameter(filmId, answers.filters)
 }
 
 cli(filmId)
+
+const sumOfDiameter = async (filmId, filters) => {
+  const film = await API.Swapi.getFilm(filmId)
+  if (!film) {
+    console.log(chalk.red('An error occured while retreiving the film information'))
+    console.log(chalk.red('Please ensure that the film id is correct'))
+
+    process.exit(1)
+  }
+
+  const planets = await Promise.all(film.planets.map(planetUrl => API.Swapi.getPlanet(planetUrl)))
+  if (planets.some(planet => !planet)) {
+    console.log(chalk.red('An error occured while retreiving the planets information'))
+    console.log(chalk.red('This could be a problem with the swapi API, try later'))
+
+    process.exit(1)
+  }
+
+  const sumOfDiameter = planets.reduce(
+    (sum, planet) =>
+      (filters.includes(Filters.mountains) ? hasMountains(planet) : true) &&
+      (filters.includes(Filters.water) ? hasWaterSurface(planet) : true)
+        ? sum + Number(planet.diameter)
+        : sum,
+    0
+  )
+
+  console.log(`Total diameter: ${sumOfDiameter}`)
+}
