4b1b3ed91944cb5eda9b1678b4b3454693e845a8
Racz Kerry <raczkerry@gmail.com>
2021-03-31 15:17:20 +0200

    Ask the user for terrains filter and miscellaneous filters
    Use the refactored sumOfDiameter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Filters } = require('./enums')
+const { MiscellaneousFilters, Terrains } = require('./enums')
 const { sumOfDiameter } = require('./utils')
 const { positiveNumberRegex } = require('./utils/regexes')
 const chalk = require('chalk')
@@ -16,20 +16,30 @@ const cli = async filmId => {
   const answers = await inquirer.prompt([
     {
       type: 'checkbox',
-      message: 'Select the filters you want to apply',
-      name: 'filters',
-      choices: Object.values(Filters),
+      message: 'Select the terrains properties you want to apply',
+      name: 'terrains',
+      choices: [new inquirer.Separator('Terrains properties'), ...Object.values(Terrains)],
+      validate: () => true
+    },
+    {
+      type: 'checkbox',
+      message: 'Select the miscellaneous properties you want to apply',
+      name: 'miscellaneous',
+      choices: [new inquirer.Separator('Terrains properties'), ...Object.values(MiscellaneousFilters)],
       validate: () => true
     }
   ])
 
-  if (!answers && !answers.filters) {
+  if (!answers || !answers.terrains || !answers.miscellaneous) {
     console.log(chalk.red('Something bad happened with the inquired module'))
 
     process.exit(1)
   }
 
-  const totalDiameter = await sumOfDiameter(filmId, answers.filters)
+  const totalDiameter = await sumOfDiameter(filmId, {
+    terrains: answers.terrains,
+    miscellaneous: answers.miscellaneous
+  })
   if (!totalDiameter) process.exit(1)
 
   console.log(`Total diameter: ${totalDiameter}`)
